d1c01ee2f91c752114aac1f83a853f91a2a46200
Aaron Swift <aaronswift72@gmail.com>
2026-03-12 15:14:03 -0500

    moving background
    finished moving background code (sickening)

--- Assets/Scripts/BackgroundManager.cs
+++ Assets/Scripts/BackgroundManager.cs
@@ -1,28 +1,45 @@
-using System.Numerics;
+using System.Linq.Expressions;
+//using System.Numerics;
 using UnityEngine;
 
 public class BackgroundManager : MonoBehaviour
 {
-    // public GameObject bgPrefab;
-    // public float speed;
-    // private GameObject[] bgs;
-    // public float pivotPoint;
+    public GameObject bgPrefab;
+    public float speed;
+    private GameObject[] bgs;
+    public float pivotPoint;
+    public float scale;
 
-    // void Start()
-    // {
-    //     bgs = new GameObject[3];
-
-    //     for (int i = 0; i < 3; i++)
-    //     {
-    //         float xPos = pivotPoint - (pivotPoint/2 * i);
-    //         float yPos = pivotPoint - (pivotPoint/2 * i);
-    //         Vector3 pos = new Vector3(0.0f, 0.0f, 0.0f);
-    //         bgs[i] = Instantiate(bgPrefab, pos, Quaternion.identity);
-    //     }
-    // }
-
-    // void Update()
-    // {
+    void Start()
+    {
+        pivotPoint = -0.32f  * 16 * scale;
+        bgPrefab.transform.localScale = new Vector3(scale, scale, scale);
         
-    // }
+        bgs = new GameObject[3];
+
+        for (int i = 0; i < 3; i++)
+        {
+            float xPos = pivotPoint - (pivotPoint/2 * i);
+            float yPos = pivotPoint - (pivotPoint/2 * i);
+            Vector2 position = new Vector2(xPos, yPos);
+            bgs[i] = Instantiate(bgPrefab, position, Quaternion.identity);
+        }
+    }
+
+    void Update()
+    {
+        for (int i = 0; i < 3; i++)
+        {
+            float xPos = bgs[i].transform.position.x + speed;
+            float yPos = bgs[i].transform.position.y + speed;
+            Vector2 position = new Vector2(xPos, yPos);
+            bgs[i].transform.position = position;
+
+            if(bgs[i].transform.position.x > -pivotPoint/2)
+            {
+                position = new Vector2(pivotPoint, pivotPoint);
+            }
+            bgs[i].transform.position = position;
+        }
+    }
 }
